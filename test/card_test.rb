2c81623f431e49a66ddfd052613f3bf5be3f07db
Michael Gatewood <michael@mngatewood.com>
2018-08-18 11:37:58 -0600

    Add value and suit

--- test/card_test.rb
+++ test/card_test.rb
@@ -8,6 +8,15 @@ class CardTest < Minitest::Test
     card = Card.new("10", "Hearts")
     assert_instance_of Card, card
   end
-
-
+  
+  def test_it_has_a_value
+    card = Card.new("10", "Hearts")
+    assert_equal card.value, "10"
+  end
+  
+  def test_it_has_a_suit
+    card = Card.new("10", "Hearts")
+    assert_equal card.suit, "Hearts"
+  end
+  
 end

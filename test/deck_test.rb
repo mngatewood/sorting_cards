d497057e3ef19117a6ae6551b3bc4f3045c02bf0
Michael Gatewood <michael@mngatewood.com>
2018-08-18 12:50:44 -0600

    Add cards

---
+++ test/deck_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+
+  def test_it_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_instance_of Deck, deck
+  end
+
+  def test_it_has_cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal deck.cards, [card_1, card_2, card_3]
+  end
+
+end

f2eb5dad2b18ff141d67246dfb689168246a03d7
Michael Gatewood <michael@mngatewood.com>
2018-08-18 13:16:02 -0600

    Adds guesses to Round

--- lib/round.rb
+++ lib/round.rb
@@ -2,10 +2,12 @@ require './lib/deck'
 
 class Round
 
-  attr_reader :deck
+  attr_reader :deck,
+              :guesses
 
   def initialize(deck)
-    @deck   = deck
+    @deck     = deck
+    @guesses  = []
   end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -12,4 +12,20 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, round
   end
 
+  def test_it_has_a_deck
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal round.deck, deck
+  end
+
+  def test_it_has_an_array_of_guesses
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal round.guesses, []
+  end
+
 end

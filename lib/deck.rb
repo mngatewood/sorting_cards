5646f921067bf511c759008414a442a96cb43c9c
Michael Gatewood <michael@mngatewood.com>
2018-08-18 18:03:26 -0600

    Add sort cards by value

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,5 @@
 require './lib/card'
+require 'pry'
 
 class Deck
 
@@ -10,6 +11,42 @@ class Deck
 
   def count
     @cards.length
+  end
+
+  def sorting_the_deck
+    sorted_deck = []
+    @cards.each_with_index do |card, i|
+      sorted_deck << card if i == 0
+      y = i
+      while y >= 0
+        sorted_deck << card if y == 0
+        if card.card_value < sorted_deck[y - 1].card_value
+          sorted_deck[y - 2] = card
+        end
+         i -= 1
+        end
+      end
+  end
+
+  def sort
+    sort_by_value
+  end
+
+  def sort_by_value
+    sorted_deck = []
+    @cards.each do | card |
+      sorted_deck << card
+      if sorted_deck.length > 1
+        j = sorted_deck.length - 1
+        while j > 0
+          if sorted_deck[j].value < sorted_deck[j-1].value
+            sorted_deck[j-1], sorted_deck[j] = sorted_deck[j], sorted_deck[j-1]
+          end
+          j -= 1
+        end
+      end
+    end
+    return sorted_deck
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -28,4 +28,13 @@ class DeckTest < Minitest::Test
     assert_equal deck.count, 3
   end
 
+  def test_it_sorts_cards
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("6", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("9", "Spades")
+    card_5 = Card.new("7", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+    assert_equal [card_1, card_3, card_2, card_5, card_4], deck.sort
+  end
 end
